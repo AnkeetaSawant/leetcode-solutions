78f6358c21243645c417b1efc15f7b360183e4c0
AnkeetaSawant <ankeeta.sawant08@gmail.com>
2026-01-02 18:43:06 +0530

    Time: 1 ms (57.00%) | Memory: 18.4 MB (74.32%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        arr = []
+        for i in range(len(nums)):
+            val = target - nums[i]
+            if val not in hash_map:
+                hash_map[nums[i]] = i
+            else:
+                return [hash_map[val], i]
+
+        return 0      
