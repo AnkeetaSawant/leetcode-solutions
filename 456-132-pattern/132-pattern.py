87c965a6751df219f2d0570874e0c75c17a1cb5b
AnkeetaSawant <ankeeta.sawant08@gmail.com>
2026-01-25 10:37:21 +0530

    Time: 57 ms (57.93%) | Memory: 36.6 MB (17.16%) - LeetSync

--- 456-132-pattern/132-pattern.py
+++ 456-132-pattern/132-pattern.py
@@ -1,14 +1,13 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
         stack = []
-        k_value = float('-inf')
-        for i in range(len(nums)-1,-1, -1):
-            if nums[i] < k_value:
+        kVal = float('-inf')
+        for i in range(len(nums)-1, -1, -1):
+            if nums[i] < kVal:
                 return True
 
-            while stack and stack[-1] < nums[i]:
-                k_value = stack.pop()
-
+            while stack and stack[-1]<nums[i]:
+                kVal = stack.pop()
             stack.append(nums[i])
 
         return False
