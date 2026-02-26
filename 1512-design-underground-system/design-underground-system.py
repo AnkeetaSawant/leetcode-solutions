4a2b108c5342633b06ee9aa0c6cecf34af3968a5
AnkeetaSawant <ankeeta.sawant08@gmail.com>
2026-02-26 20:18:42 +0530

    Time: 22 ms (66.78%) | Memory: 29.3 MB (55.23%) - LeetSync

---
+++ 1512-design-underground-system/design-underground-system.py
@@ -0,0 +1,38 @@
+class Customer:
+    def __init__(self, cardId):
+        self.cardId = cardId
+
+class UndergroundSystem:
+
+    def __init__(self):
+        self.total_time_map = {}
+        self.checkin_time_per_cust = {}
+
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
+        if id in self.checkin_time_per_cust:
+            print(f"Customer already checkin at {self.checkin_time_per_cust[id][1]}")
+        self.checkin_time_per_cust[id] = (stationName,t)
+
+
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        if id in self.checkin_time_per_cust:
+            key = (self.checkin_time_per_cust[id][0], stationName)
+            prev_distance, prev_count = self.total_time_map.get(key,(0,0))
+            self.total_time_map[key] = (
+                prev_distance + (t -  self.checkin_time_per_cust[id][1]),
+                prev_count + 1)
+            del self.checkin_time_per_cust[id]
+        else:
+            print(f"No record found for that Customer checkin in any Station")
+
+
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        if (startStation, endStation) in self.total_time_map:
+            return (self.total_time_map[(startStation, endStation)][0]/self.total_time_map[(startStation, endStation)][1])
+
+
+# Your UndergroundSystem object will be instantiated and called as such:
+# obj = UndergroundSystem()
+# obj.checkIn(id,stationName,t)
+# obj.checkOut(id,stationName,t)
+# param_3 = obj.getAverageTime(startStation,endStation)
