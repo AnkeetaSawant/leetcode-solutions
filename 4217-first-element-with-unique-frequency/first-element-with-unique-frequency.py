127b1f1abbb7af59ba8fbcc8d430d317595bf51c
AnkeetaSawant <ankeeta.sawant08@gmail.com>
2026-03-08 10:20:35 +0530

    Time: 287 ms (13.93%) | Memory: 41.3 MB (69.55%) - LeetSync

---
+++ 4217-first-element-with-unique-frequency/first-element-with-unique-frequency.py
@@ -0,0 +1,13 @@
+class Solution:
+    def firstUniqueFreq(self, nums: List[int]) -> int:
+        freq_num = defaultdict(int)
+        freqCount = defaultdict(int)
+        for num in nums:
+            freq_num[num] += 1
+        for key,value in freq_num.items():
+            freqCount[value] += 1
+        
+        for num in nums:
+            if freqCount[freq_num[num]] == 1:
+                return num
+        return -1
